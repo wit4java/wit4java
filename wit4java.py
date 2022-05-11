cf43aee6acff799c12077c3f92e0b34fb924eb47
Joss Moffatt <jossmoff@hotmail.co.uk>
2022-05-11 19:23:22 +0100

    Adding additional parts for benchexec to run properly

--- wit4java.py
+++ wit4java.py
@@ -25,9 +25,10 @@ try:
         print("wit4java version: 2.0")
         witness_file_dir = sys.argv[2]
         print("witness: ", witness_file_dir)
-        print(os.getcwd())
-        java_common_path = sys.argv[3]
-        java_files_path = sys.argv[4]
+        # Have to do this for benchexec
+        # TODO: better name
+        java_commons_paths = sys.argv[3:-1]
+        java_files_path = sys.argv[-1]
         # Need to preprocess and move to current directory to utilise mockito
         teardown_files = process_java_files(java_files_path)
         # Process files to get maps
@@ -38,16 +39,17 @@ try:
         type_assumption_pairs = []
         for file_name, line_type_map in file_line_type_map.items():
             for line, type in line_type_map.items():
-                # Check non mapping
+                # Assure every nondet call has at least one valid assumption
                 if file_name in file_line_assumption_map and line in file_line_assumption_map[file_name]:
                     assumptions = file_line_assumption_map[file_name][line]
                     pairs = [(type, assumption) for assumption in assumptions]
                     type_assumption_pairs.extend(pairs)
+
         # Check for each type from java file we have an assumptions
         utb = UnitTestBuilder(type_assumption_pairs)
         utb.build_unit_test('Test.java')
 
-        cmd = "javac -cp {} Test.java".format(os.getenv('CLASSPATH'))
+        cmd = "javac -cp {0}:{1} Test.java".format(os.getenv('CLASSPATH'), ':'.join(java_commons_paths[3:-1]))
         subprocess.Popen(cmd, shell=True).wait()
 
         cmd1 = "java -ea Test"
@@ -55,7 +57,7 @@ try:
 
         # Delete copied files
         for file in teardown_files:
-            os.remove(file)
+           os.remove(file)
 
 
 except BaseException as e:
