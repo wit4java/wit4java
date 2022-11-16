d0d48aba09763e0cc5b0fe2daad052943694b08f
Joss Moffatt <josshmoffatt@gmail.com>
2022-11-16 17:02:57 +0000

    :sparkles: Adding ability to run in current working directory

--- wit4java/wit4java.py
+++ wit4java/wit4java.py
@@ -63,7 +63,11 @@ def create_argument_parser() -> argparse.ArgumentParser:
         help='Path to the witness file. Must conform to the exchange format'
     )
 
-    parser.add_argument('--local-dir', help='perform all of the processing in the current directory', action='store_true')
+    parser.add_argument(
+        '--local-dir',
+        help='perform all of the processing in the current directory',
+        action='store_true'
+    )
 
     parser.add_argument(
         "--version", action="version", version="%(prog)s " + __version__
